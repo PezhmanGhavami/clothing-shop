5e78fbcffbcb26c182d52a8c967478e6b1a5555e
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-06-27 05:49:53 +0000

    Added some basic stylings - added robot font to the project made some links in navbar - made a seeder to seed the db with some categories, brands and items

---
+++ backend/seeder/seedDB.js
@@ -0,0 +1,93 @@
+const prisma = require("../utils/prisma-client");
+const SHOP_DATA = require("./shop-data");
+const BRANDS = require("./company-names.json");
+
+async function seedDB() {
+  const deleteItems = prisma.item.deleteMany({});
+  const deleteCategories = prisma.category.deleteMany({});
+  const deleteBrands = prisma.brand.deleteMany({});
+
+  await prisma.$transaction([
+    deleteItems,
+    deleteCategories,
+    deleteBrands,
+  ]);
+
+  await prisma.brand.createMany({
+    data: BRANDS,
+    skipDuplicates: true,
+  });
+
+  const seedCategory = await prisma.category.create({
+    data: {
+      name: "seed",
+    },
+  });
+
+  // const items = [];
+
+  for (const category of SHOP_DATA) {
+    const newCategory = await prisma.category.create({
+      data: {
+        name: category.title,
+      },
+    });
+
+    const brands = await prisma.brand.findMany({});
+    for (const itemOfCategory of category.items) {
+      const brandIndex = Math.floor(Math.random() * 4);
+      // items.push({
+      //   name: itemOfCategory.name,
+      //   price: itemOfCategory.price,
+      //   images: [itemOfCategory.imageUrl],
+      //   brandId: brands[brandIndex].id,
+      //   currentInventory: Math.floor(Math.random() * 20),
+      //   description:
+      //     "test description for an item that is of brand test brand",
+      //   categories: {
+      //     set: [
+      //       { id: seedCategory.id },
+      //       { id: newCategory.id },
+      //     ],
+      //   },
+      // });
+      await prisma.item.create({
+        data: {
+          name: itemOfCategory.name,
+          price: itemOfCategory.price,
+          images: [itemOfCategory.imageUrl],
+          brand: { connect: { id: brands[brandIndex].id } },
+          currentInventory: Math.floor(Math.random() * 20),
+          description:
+            "test description for an item that is of brand test brand",
+          categories: {
+            connectOrCreate: [
+              {
+                create: {
+                  name: "seed",
+                },
+                where: {
+                  name: "seed",
+                },
+              },
+              {
+                create: {
+                  name: category.title,
+                },
+                where: {
+                  name: category.title,
+                },
+              },
+            ],
+          },
+        },
+      });
+    }
+  }
+  // await prisma.item.createMany(
+  //   { include: { categories: true } },
+  //   { data: items }
+  // );
+}
+
+seedDB();
--- frontend/src/components/navbar/navbar.component.tsx
+++ frontend/src/components/navbar/navbar.component.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
+import "./navbar.styles.css";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <Link className="logo-container" to={"/"}>
+        Home
+      </Link>
+      <div className="nav-links">
+        <Link to={"/categories"}>Categories</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
