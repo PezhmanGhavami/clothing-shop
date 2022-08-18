c8218b0b594e4d7613fa7dc197567c0d0233c5c0
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-08-18 10:41:17 +0000

    rested the main branch to aa0adb4 - updated all packages - fixed the styling for not-found page to correctly center it's content based on the changes done to layout

--- frontend/src/pages/not-found/not-found.tsx
+++ frontend/src/pages/not-found/not-found.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div className="h-full flex flex-col justify-center items-center">
+    <div className="h-[85vh] flex flex-col justify-center items-center">
       <p className="text-4xl pb-4">There's nothing here!</p>
       <Link className="pt-4 text-slate-400 hover:underline hover:text-slate-100" to={"/"}>Go Back Home</Link>
     </div>
