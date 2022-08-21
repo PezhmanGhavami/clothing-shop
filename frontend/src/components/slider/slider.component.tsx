8ad76e311d17dc0a476177fab77c5d832a646ad9
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-08-21 08:11:24 +0000

    Made a slide component and imported it into slider component - changed the prisma schema for item model to have a discountedPrice, offer boolean and a featured boolean which is temprory, todo note in the schema file

--- frontend/src/components/slider/slider.component.tsx
+++ frontend/src/components/slider/slider.component.tsx
@@ -1,25 +1,13 @@
+import Slide from "../slide/slide.component";
+
 function Slider() {
   return (
+    // Slider container
     <div className="overflow-x-hidden">
-      <div className="h-80 inline-flex transform translate-x-0 cursor-grab">
-        <div className="w-screen ">
-          <img
-            src="https://images.unsplash.com/photo-1542219550-2da790bf52e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
-            alt=""
-          />
-        </div>
-        <div className="w-screen">
-          <p>Slide B</p>
-        </div>
-        <div className="w-screen">
-          <p>Slide C</p>
-        </div>
-        <div className="w-screen">
-          <p>Slide D</p>
-        </div>
-        <div className="w-screen">
-          <p>Slide E</p>
-        </div>
+      {/* Slider */}
+      <div className="h-80 inline-flex cursor-grab">
+        {/* Slide */}
+          
       </div>
     </div>
   );
