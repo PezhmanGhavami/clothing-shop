ecbe0b03a32b0de8b168550792f850c3fb3db047
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-09-20 11:49:41 +0000

    Modifed the product card container prop type, started working on product page; Fetched data from db, added the related products component; Things left are the product preview component and a review preview

--- src/components/product-card-container/product-card-container.component.tsx
+++ src/components/product-card-container/product-card-container.component.tsx
@@ -1,28 +1,31 @@
 import Link from "next/link";
+import { Item } from "@prisma/client";
 
 import ProductCard from "../product-card/product-card.component";
 
-import { ICategory } from "../../pages/categories/index";
-
+export interface IProductCardContainerData {
+  name: string;
+  items: Item[];
+}
 interface IProductCardContainer {
-  category: ICategory;
+  productGroup: IProductCardContainerData;
   isPreview: boolean;
 }
 
 const ProductCardContainer = ({
-  category,
+  productGroup,
   isPreview,
 }: IProductCardContainer) => {
   return (
     <div className="mx-auto max-w-2xl py-6 px-4 sm:pt-10 lg:max-w-7xl lg:px-8">
       <div className="flex justify-between items-center">
         <h2 className="capitalize text-2xl font-bold tracking-tight">
-          {category.name}
+          {productGroup.name}
         </h2>
         {isPreview && (
           <Link
             href={
-              "/categories/" + category.name.toLowerCase()
+              "/categories/" + productGroup.name.toLowerCase()
             }
           >
             <a className="text-blue-700 dark:text-blue-400 hover:underline">
@@ -34,7 +37,7 @@ const ProductCardContainer = ({
       </div>
 
       <div className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-6 xl:gap-x-8">
-        {category.items.map((product) => (
+        {productGroup.items.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
