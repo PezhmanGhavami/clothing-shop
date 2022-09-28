54db3a16fc5ca3f3871666c9e4aa1d6b09e9dd8f
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-09-28 12:18:20 +0000

    Finished the general design of the slideshow

--- src/components/slideshow/slideshow.component.tsx
+++ src/components/slideshow/slideshow.component.tsx
@@ -22,10 +22,15 @@ const Slideshow = ({ slides }: ISlideshow) => {
           &#10095;
         </span>
       </div>
-      <div className="absolute bottom-0 w-full flex justify-center items-center text-white">
-        <span>&#9899;</span>
-        <p>button</p>
-        <p>button</p>
+      <div className="absolute bottom-0 w-full flex justify-center items-center text-white py-2">
+        {slides.map((slide) => (
+          <div
+            key={slide.id}
+            className="cursor-pointer py-2 mx-2"
+          >
+            <div className="h-1 w-7 border-b" />
+          </div>
+        ))}
       </div>
     </div>
   );
