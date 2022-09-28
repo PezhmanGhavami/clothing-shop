33b83f74fe7d8a38d7ba2c1cbf589e520a7c80f4
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-09-28 16:47:20 +0000

    The slideshow is working - There are animations and some styling left - I also need to implement touch and mouse grab slide change

--- src/components/slideshow/slideshow.component.tsx
+++ src/components/slideshow/slideshow.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Slide, { slide } from "../slide/slide.component";
 
@@ -6,24 +6,65 @@ interface ISlideshow {
   slides: (slide & { id: number })[];
 }
 
+const getTranslate = (slide: number) => {
+  return -slide * 100;
+};
+
 const Slideshow = ({ slides }: ISlideshow) => {
-  const [currentSlide, setCurrentSlide] = useState(
-    slides[0].id
+  const [currentSlide, setCurrentSlide] = useState(1);
+  // const [currentSlideID, setCurrentSlideID] = useState(1);
+  const [currentTranslate, setcurrentTranslate] = useState(
+    getTranslate(currentSlide)
   );
+
+  useEffect(() => {
+    const timer = setInterval(handleNextSlide, 3000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    setcurrentTranslate(getTranslate(currentSlide));
+    if (currentSlide <= 0) setCurrentSlide(slides.length);
+    if (currentSlide >= slides.length + 1)
+      setCurrentSlide(1);
+  }, [currentSlide, slides.length]);
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prev) => prev + 1);
+  };
+  const handlePreviousSlide = () => {
+    setCurrentSlide((prev) => prev - 1);
+  };
+
   return (
     <div className="relative bg-gray-200 dark:bg-slate-500 text-white h-[65vh] overflow-hidden">
       {/* Slides container */}
-      <div className="h-full inline-flex">
+      <div
+        className="h-full inline-flex"
+        style={{
+          transform: `translateX(${currentTranslate}vw)`,
+        }}
+      >
+        <Slide slide={slides[slides.length - 1]} />
         {slides.map((slide) => (
           <Slide key={slide.id} slide={slide} />
         ))}
+        <Slide slide={slides[0]} />
       </div>
       {/* The buttons to change slides */}
       <div className="absolute w-full inset-0 flex justify-between items-center text-2xl">
-        <span className="cursor-pointer py-4 px-7">
+        <span
+          onClick={handlePreviousSlide}
+          className="cursor-pointer py-4 px-7"
+        >
           &#10094;
         </span>
-        <span className="cursor-pointer py-4 px-7">
+        <span
+          onClick={handleNextSlide}
+          className="cursor-pointer py-4 px-7"
+        >
           &#10095;
         </span>
       </div>
