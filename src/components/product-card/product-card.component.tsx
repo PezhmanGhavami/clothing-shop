14c71aa29f743df4a71d0b97e3c1db2bed79f157
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-10-06 18:26:14 +0000

    Added a span to show the if the item is in inventory or in it's last inventory in the product-card.component

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -22,14 +22,29 @@ const ProductCard = ({
     <div>
       <Link href={"/product/" + product.id}>
         <a>
-          <div className="relative h-72 w-full overflow-hidden rounded-md bg-gray-200 hover:opacity-75">
+          <div className="relative h-72 w-full overflow-hidden rounded-md bg-gray-200">
+            <span
+              className={`px-2 rounded-md absolute top-1 left-1 z-10 text-white ${
+                product.currentInventory <= 0
+                  ? "bg-red-600"
+                  : product.currentInventory <= 3
+                  ? "bg-yellow-600"
+                  : ""
+              }`}
+            >
+              {product.currentInventory <= 0
+                ? "Out of stock"
+                : product.currentInventory <= 3
+                ? `Last ${product.currentInventory} in stock`
+                : ""}
+            </span>
             <Image
               src={product.images[0]}
               alt={product.name}
               layout="fill"
               quality={100}
               priority
-              className="object-cover object-center dark:brightness-90"
+              className="object-cover object-center dark:brightness-90 hover:opacity-75"
             />
           </div>
           <div className="mt-4 flex justify-between">
