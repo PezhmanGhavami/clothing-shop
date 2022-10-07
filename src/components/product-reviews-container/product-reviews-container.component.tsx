fcbb0968a12c56d7b9f852d70cf8b697019a0ed6
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-10-07 15:31:28 +0000

    Added scroll margin to the review section so it accouts for the sticky navbar

--- src/components/product-reviews-container/product-reviews-container.component.tsx
+++ src/components/product-reviews-container/product-reviews-container.component.tsx
@@ -11,7 +11,7 @@ const ProductReviewsContainer = ({
   return (
     <div
       id="reviews-section"
-      className="sm:w-3/4 lg:w-2/4 mx-auto p-8"
+      className="sm:w-3/4 lg:w-2/4 mx-auto p-8 scroll-mt-20"
     >
       <h2 className="text-3xl text-center p-4">
         Customer Reviews
