4aec207231d34336e17268559539545fd0599457
Pezhman Ghavami <73465670+pezhmanghavami@users.noreply.github.com>
2022-10-07 12:44:50 +0000

    Re added the id for the reviews container

--- src/components/product-reviews-container/product-reviews-container.component.tsx
+++ src/components/product-reviews-container/product-reviews-container.component.tsx
@@ -9,7 +9,10 @@ const ProductReviewsContainer = ({
   reviews,
 }: IProductReviewsContainer) => {
   return (
-    <div className="sm:w-3/4 lg:w-2/4 mx-auto p-8">
+    <div
+      id="reviews-section"
+      className="sm:w-3/4 lg:w-2/4 mx-auto p-8"
+    >
       <h2 className="text-3xl text-center p-4">
         Customer Reviews
       </h2>
