7a1eb63f82192ca44167967ee69b94709156e511
Pejman Ghavami <pejmanghavami@gmail.com>
2023-01-30 08:31:31 -0500

    Changed my approach on the way I styled toastify which resulted in cleaning up layout by removing the unnecessary JS and making toastify, toasts react to context wide theme change

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,14 +1,8 @@
-import { useState, useEffect, useContext } from "react";
-import {
-  ToastContainer,
-  toast,
-  Theme,
-} from "react-toastify";
+import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 import Navbar from "../navbar/navbar.component";
 import Footer from "../footer/footer.component";
-
-import { ThemeContext } from "../../context/theme.context";
 
 interface ILayout {
   children: React.ReactNode;
@@ -34,30 +28,14 @@ const CustomeToast = () => {
 };
 
 const Layout = ({ children }: ILayout) => {
-  const [showToast, setShowToast] = useState(true);
-  const { theme } = useContext(ThemeContext);
-
   useEffect(() => {
-    if (theme === "" || !showToast) return;
     toast.info(CustomeToast, {
       autoClose: false,
     });
-    setShowToast(false);
-  }, [theme, showToast]);
+  }, []);
   return (
     <div className="min-h-screen">
-      <ToastContainer
-        theme={theme as Theme}
-        toastStyle={{
-          backgroundColor:
-            theme === "dark" ? "#1e293b" : "#f5f5f5",
-          color: theme === "dark" ? "#fff" : "#0f172a",
-        }}
-        bodyStyle={{
-          backgroundColor:
-            theme === "dark" ? "#1e293b" : "#f5f5f5",
-        }}
-      />
+      <ToastContainer />
       <Navbar />
       <main className="border-y dark:border-y-slate-700 min-h-[85vh]">
         {children}
