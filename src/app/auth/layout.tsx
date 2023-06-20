c972474b9938d1a5b3f0d07ba7a361dde0fbec1d
Pezhman Ghavami <pejmanghavami@gmail.com>
2023-06-20 12:48:03 +0000

    Removed an unnecessary div from auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,14 +2,12 @@ import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-full grow bg-white text-slate-900 dark:bg-slate-900 dark:text-white">
-      <div className="flex h-full flex-col justify-between">
-        <div className="flex flex-col items-center justify-start pt-8">
-          <div className="pb-6 pt-8 text-3xl font-medium uppercase tracking-wide lg:p-4">
-            <Link href={"/"}>Clothing Shop</Link>
-          </div>
-          {children}
+    <div className="flex h-full grow flex-col justify-between bg-white text-slate-900 dark:bg-slate-900 dark:text-white">
+      <div className="flex flex-col items-center justify-start pt-8">
+        <div className="pb-6 pt-8 text-3xl font-medium uppercase tracking-wide lg:p-4">
+          <Link href={"/"}>Clothing Shop</Link>
         </div>
+        {children}
       </div>
     </div>
   );
