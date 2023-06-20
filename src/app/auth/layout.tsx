cddb3c25dc884e87bef9eb08207c2197d3aad840
Pezhman Ghavami <pejmanghavami@gmail.com>
2023-06-20 13:01:33 +0000

    Added a loading state into the auth layout to handle unauthorized content flashing

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,22 @@
+"use client";
+
 import Link from "next/link";
 
+import Loading from "@/components/loading/loading.component";
+
+import useUser from "../../hooks/useUser";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { user } = useUser();
+
+  if (!user || user.isLoggedIn) {
+    return (
+      <div className="mx-auto mt-96 text-3xl">
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <div className="flex h-full grow flex-col justify-between bg-white text-slate-900 dark:bg-slate-900 dark:text-white">
       <div className="flex flex-col items-center justify-start pt-8">
