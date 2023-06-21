63a2b1e85d2cce43fce087ad836cf4b5b51e9cd2
Pezhman Ghavami <pejmanghavami@gmail.com>
2023-06-21 14:28:58 +0000

    Fixed a visual bug in the not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
 
+import Navbar from "@/components/navbar/navbar.component";
+
 export const metadata = {
   title: "Page not found",
 };
 
 const NotFound = () => {
   return (
-    <div className="flex h-[85vh] flex-col items-center justify-center">
-      <p className="pb-4 text-4xl">There&apos;s nothing here!</p>
-      <Link
-        href={"/"}
-        className="pt-4 text-blue-700 hover:underline dark:text-blue-400"
-      >
-        Go Back Home <span aria-hidden="true">→</span>
-      </Link>
-    </div>
+    <>
+      <Navbar />
+      <div className="flex h-full grow flex-col items-center justify-center">
+        <p className="pb-4 text-4xl">There&apos;s nothing here!</p>
+        <Link
+          href={"/"}
+          className="pt-4 text-blue-700 hover:underline dark:text-blue-400"
+        >
+          Go Back Home <span aria-hidden="true">→</span>
+        </Link>
+      </div>
+    </>
   );
 };
 
