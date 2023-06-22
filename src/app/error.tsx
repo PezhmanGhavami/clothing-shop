e5511a6dd73be7bcd77fac0b7f2d44a24df3c134
Pezhman Ghavami <pejmanghavami@gmail.com>
2023-06-22 17:43:53 +0000

    Added an error component in the app dir

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+// TODO - make an endpoint and send the error data there
+const RootError = () => {
+  return (
+    <div className="mx-auto mt-96 h-full grow text-3xl">
+      <p>Something went wrong...</p>
+    </div>
+  );
+};
+export default RootError;
